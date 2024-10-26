84c1f182a4b89ed456dcd7e7c6844120f49d0eee
Khabweri <114258686+Khabweri@users.noreply.github.com>
2024-10-26 20:09:12 +0300

    Add files via upload

---
+++ src/CoreComponentRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MehediIitdu\CoreComponentRepository;
+use App\Models\Addon;
+use Cache;
+
+class CoreComponentRepository
+{
+    public static function instantiateShopRepository() {
+        
+    }
+
+    protected static function finalizeRepository($rn) {
+        
+    }
+
+    public static function initializeCache() {
+        foreach(Addon::all() as $addon){
+
+            Cache::rememberForever($addon->unique_identifier.'-purchased', function () {
+                return 'yes'; 
+            });
+        }
+    }
+
+    public static function finalizeCache($addon){
+        $addon->activated = 1; 
+        $addon->save();
+    
+        flash($addon->name . ' has been activated successfully.')->success();
+        
+        return true;
+    }
+    
+}
